60e24dc57ec2abf0682ca5a32c0e7ba8338168e1
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-09 20:02:13 +0100

    Edit test url and title

--- spec/features/deleting_a_bookmark_spec.rb
+++ spec/features/deleting_a_bookmark_spec.rb
@@ -1,17 +1,17 @@
 feature "Deleting a bookmark" do 
   scenario "When a user wants to remove a bookmark" do 
     # create test data
-    Bookmark.create(url: 'http://example.org', title: "Example Domain" )
+    Bookmark.create(url: 'http://www.google.com', title: 'Google' )
     visit ('/bookmarks')
 
     # check that the test data is on the page
-    expect(page).to have_link("Example Domain", href: 'http://example.org')
+    expect(page).to have_link('Google', href: 'http://www.google.com')
 
     # click the delete button of the first element in Bookmark class
     first('.bookmark').click_button 'Delete'
 
     # check that the test data has been removed from the page
-    expect(page).not_to have_link("Example Domain", href: 'http://example.org')
+    expect(page).not_to have_link('Google', href: 'http://www.google.com')
     # make sure that checking the right page /bookmarks
     expect(current_path).to eq '/bookmarks'
   end 
