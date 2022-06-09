d2f2b5d4f78e6c606250e580becd8a87a557891c
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-09 19:10:54 +0100

    Add SQL injection PG gem

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -25,11 +25,15 @@ class Bookmark
 
   def self.create(url:, title:)
     if ENV['ENVIRONMENT'] == 'test'
-      conn = PG.connect(dbname: 'bookmark_manager_test')
+     conn = PG.connect(dbname: 'bookmark_manager_test')
     else
-      conn = PG.connect(dbname: 'bookmark_manager')
+     conn = PG.connect(dbname: 'bookmark_manager')
     end
-    result = conn.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
+    # The first argument is our SQL query template
+      # The second argument is the 'params' referred to in exec_params
+      # $1 refers to the first item in the params array
+      # $2 refers to the second item in the params array
+    result = conn.exec_params("INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;",  [url, title])
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
-  end 
+end
 end
